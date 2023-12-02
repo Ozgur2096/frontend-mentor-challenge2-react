a073f232a11a0575cb04c23fd9a79176219ee6f7
Ozgur Ozhan <ozgur2096@gmail.com>
2023-12-02 16:05:55 +0100

    selectPlan.tsx modified

--- src/components/MainContentElements/SelectPlan.tsx
+++ src/components/MainContentElements/SelectPlan.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export const SelectPlan = () => {
-  return <main className='main-content-container'>SelectPlan</main>;
+  return (
+    <main className='main-content-container'>
+      <h2>Select your plan</h2>
+      <p>You have the option of monthly or yearly billing.</p>
+      <div>
+        <div className='select-plan-box-container'>
+          <div className='select-plan-box'>Arcade</div>
+          <div className='select-plan-box'>Advanced</div>
+          <div className='select-plan-box'>Pro</div>
+        </div>
+        <div className='select-plan-toggle'>
+          <p>Monthly</p>
+          <div className='select-plan-toggle-button'></div>
+          <p>Yearly</p>
+        </div>
+      </div>
+    </main>
+  );
 };
