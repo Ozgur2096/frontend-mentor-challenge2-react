bd1c18f85b48d6b9e362e2518efae2f260047ee3
Ozgur Ozhan <ozgur2096@gmail.com>
2023-12-02 15:59:22 +0100

    personalInfo.tsx modified

--- src/components/MainContentElements/PersonalInfo.tsx
+++ src/components/MainContentElements/PersonalInfo.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
-
 export const PersonalInfo = () => {
-  return <main className='main-content-container'>PersonalInfo</main>;
+  return (
+    <main className='main-content-container'>
+      <h2>Personal info</h2>
+      <p>Please provide your name, email address, and phone number.</p>
+      <div>
+        <div>
+          <label htmlFor='name'>Name</label>
+          <input type='text' id='name' placeholder='e.g. Stephen King' />
+        </div>
+        <div>
+          <label htmlFor='email'>Email Address</label>
+          <input
+            type='email'
+            id='email'
+            placeholder='e.g. stephenking@lorem.com'
+          />
+        </div>
+        <div>
+          <label htmlFor='tel'>Phone Number</label>
+          <input type='tel' id='tel' placeholder='e.g. +1 234 567 890' />
+        </div>
+      </div>
+    </main>
+  );
 };
