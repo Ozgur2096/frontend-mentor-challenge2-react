cfb5d48b09c36e31ac196f8784f31f9ffe441c4c
Ozgur Ozhan <ozgur2096@gmail.com>
2023-12-02 16:13:48 +0100

    addons modified

--- src/components/MainContentElements/AddOns.tsx
+++ src/components/MainContentElements/AddOns.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export const AddOns = () => {
-  return <main className='main-content-container'>AddOns</main>;
+  return (
+    <main className='main-content-container'>
+      <h2>Pick add-ons</h2>
+      <p>Add-ons help enhance your gaming experience.</p>
+      <div>
+        <div>Online service Access to multiplayer games +$1/mo</div>
+        <div>Larger storage Extra 1TB of cloud save +$2/mo</div>
+        <div>Customizable Profile Custom theme on your profile +$2/mo</div>
+      </div>
+    </main>
+  );
 };
--- src/components/MainContentElements/Summary.tsx
+++ src/components/MainContentElements/Summary.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export const Summary = () => {
-  return <main className='main-content-container'>Summary</main>;
+  return (
+    <main className='main-content-container'>
+      <h2>Finishing up</h2>
+      <p>Double-check everything looks OK before confirming.</p>
+    </main>
+  );
 };
