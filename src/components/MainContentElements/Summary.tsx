0c9048cc61ba6bdace9b01f3246cb604b863be9c
Ozgur Ozhan <ozgur2096@gmail.com>
2023-12-04 21:27:19 +0100

    summary.tsx modified

--- src/components/MainContentElements/Summary.tsx
+++ src/components/MainContentElements/Summary.tsx
@@ -5,6 +5,13 @@ export const Summary = () => {
     <main className='main-content-container'>
       <h2>Finishing up</h2>
       <p>Double-check everything looks OK before confirming.</p>
+      <div>
+        <div className='main-price'></div>
+        <div className='extra-price'></div>
+      </div>
+      <div className='total-price'>
+        <div>Total (per month)</div>
+      </div>
     </main>
   );
 };
