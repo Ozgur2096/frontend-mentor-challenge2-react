a2619acdb61d27f636cd299ef138f13755900038
Ozgur Ozhan <ozgur2096@gmail.com>
2023-12-05 17:33:27 +0100

    constants.tsx added

---
+++ src/constants.tsx
@@ -0,0 +1,36 @@
+export const arcade = {
+  prices: {
+    monthlyPrice: 9,
+    yearlyPrice: 90,
+  },
+};
+export const advanced = {
+  prices: {
+    monthlyPrice: 12,
+    yearlyPrice: 120,
+  },
+};
+export const pro = {
+  prices: {
+    monthlyPrice: 15,
+    yearlyPrice: 150,
+  },
+};
+export const onlineService = {
+  prices: {
+    monthlyPrice: 1,
+    yearlyPrice: 10,
+  },
+};
+export const largerStorage = {
+  prices: {
+    monthlyPrice: 2,
+    yearlyPrice: 20,
+  },
+};
+export const customizableProfile = {
+  prices: {
+    monthlyPrice: 2,
+    yearlyPrice: 20,
+  },
+};
