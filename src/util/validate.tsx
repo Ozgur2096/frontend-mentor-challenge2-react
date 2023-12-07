a5b0bc6f4ccdc0b2d0607f14e9b7036e59c3e972
Ozgur Ozhan <ozgur2096@gmail.com>
2023-12-07 16:50:20 +0100

    validation code will continue

--- src/util/validate.tsx
+++ src/util/validate.tsx
@@ -1,1 +1,8 @@
+// - Receive form validation messages if:
+//   - A field has been missed
+//   - The email address is not formatted correctly
+//   - A step is submitted, but no selection has been made
+
+// do not forget yup or zod
+
 export const validate = () => {};
